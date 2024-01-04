889d5b4cc6c43c8de75bab736ebf5911360e5686
Jesse Reichman <14865360+jreichman@users.noreply.github.com>
2024-01-04 12:47:12 +1030

    Linting changes

--- setup.py
+++ setup.py
@@ -1,41 +1,25 @@
 from setuptools import setup, find_packages
 import os
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     README = readme_file.read()
 
 setup_args = {
-    'name': 'bdast',
-    'version': os.environ['BUILD_VERSION'],
-    'description': 'Build and Deployment Assistant',
-    'long_description_content_type': 'text/markdown',
-    'long_description': README,
-    'license': 'MIT',
-    'packages': find_packages(
-        where='src',
-        include=[
-            'bdast',
-            'bdast.*'
-        ]
-    ),
-    'author': 'Jesse Reichman',
-    'keywords': [ 'Build', 'Deployment', 'Assistant' ],
-    'url': 'https://github.com/archmachina/bdast',
-    'download_url': 'https://pypi.org/project/bdast/',
-    'entry_points': {
-        'console_scripts': [
-            'bdast = bdast:main'
-        ]
-    },
-    'package_dir': {
-        '': 'src'
-    },
-    'install_requires': [
-        'requests>=2.0.0',
-        'PyYAML>=5.0.0'
-    ]
+    "name": "bdast",
+    "version": os.environ["BUILD_VERSION"],
+    "description": "Build and Deployment Assistant",
+    "long_description_content_type": "text/markdown",
+    "long_description": README,
+    "license": "MIT",
+    "packages": find_packages(where="src", include=["bdast", "bdast.*"]),
+    "author": "Jesse Reichman",
+    "keywords": ["Build", "Deployment", "Assistant"],
+    "url": "https://github.com/archmachina/bdast",
+    "download_url": "https://pypi.org/project/bdast/",
+    "entry_points": {"console_scripts": ["bdast = bdast:main"]},
+    "package_dir": {"": "src"},
+    "install_requires": ["requests>=2.0.0", "PyYAML>=5.0.0"],
 }
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args, include_package_data=True)
